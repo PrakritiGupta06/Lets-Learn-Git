06822c43bd3b4679f4e21d855f80bf74b9a02fbb
Prakriti Gupta <prakritigupta0601@gmail.com>
2021-07-11 23:12:22 +0530

    added message in forms.html

---
+++ sum.java
@@ -0,0 +1,10 @@
+package LetsLearnGit;
+
+public class Sum {
+    private static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        System.out.println(a+b);
+        
+    }
+}
